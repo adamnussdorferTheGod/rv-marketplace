486e63764a8ac185539c9a24e15e67243c45a07a
Adam <adam@Adams-MacBook-Pro.local>
2026-02-26 10:10:44 -0800

    feat(22-02): wire OwnershipCards into HomePage
    - Import and render OwnershipCards after FeaturedListings in second .content block
    - Section order: FeaturedListings > OwnershipCards (SellingSection to be added by Plan 01)

--- components/pages/HomePage/HomePage.tsx
+++ components/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import HeroBanner from './HeroBanner/HeroBanner';
 import HandPickedSection from './HandPickedSection/HandPickedSection';
 import DealerShowcase from './DealerShowcase/DealerShowcase';
 import FeaturedListings from './FeaturedListings/FeaturedListings';
+import OwnershipCards from './OwnershipCards/OwnershipCards';
 import styles from './HomePage.module.css';
 
 export default function HomePage() {
@@ -14,6 +15,7 @@ export default function HomePage() {
       <DealerShowcase />
       <div className={styles.content}>
         <FeaturedListings />
+        <OwnershipCards />
       </div>
     </div>
   );
